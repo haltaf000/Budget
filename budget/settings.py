b7c5dbc42c3562d239943664a87209bf57a54ef3
Your Name <you@example.com>
2023-01-20 14:33:19 -0500

    added url settings

--- budget/settings.py
+++ budget/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-4g2!rhf8(y!z%$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-8a1c.up.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-8a1c.up.railway.app']
+
+
 
 AUTH_USER_MODEL = 'base.User'
 
